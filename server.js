69b96ae4ef331645acaae93a9f49c0ac660176a9
Nick Clear <nickclear22@gmail.com>
2018-06-17 18:47:28 -0700

    connected css files to server. added check statement for empty input vals.

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static('app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
